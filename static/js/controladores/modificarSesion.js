9564aa8454ae923cb5355f58002a224debcefc11
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-05-05 23:56:28 -0400

    Mejoras

--- static/js/controladores/modificarSesion.js
+++ static/js/controladores/modificarSesion.js
@@ -1,31 +1,31 @@
-const formMiCuenta = document.getElementById('micuenta-form');
+document.addEventListener("DOMContentLoaded", function (event) {
+  const formMiCuenta = document.getElementById('micuenta-form');
 
-formMiCuenta.addEventListener('submit', function (event) {
-  event.preventDefault();
-  
-});
+  formMiCuenta.addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
 
-addEventListener("DOMContentLoaded", (event) => {
+  function obtenerInformacion() {
+    return fetch('.././core/cuentas/obtener_info.php', {
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => response.json())
+      .catch(error => console.error(error));
+  }
+
+  function establecerValoresFormulario(json) {
+    for (let key in json) {
+      const input = formMiCuenta.querySelector(`[name="${key}"]`);
+      if (input) {
+        input.value = json[key];
+      }
+    }
+  }
+
+  // Obtiene información y establece valores del formulario
   obtenerInformacion()
     .then(json => establecerValoresFormulario(json))
     .catch(error => console.error(error));
-})
-
-function obtenerInformacion() {
-  return fetch('.././core/cuentas/obtener_info.php', {
-    method: 'POST',
-    body: JSON.stringify({}),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
-}
-
-function establecerValoresFormulario(json) {
-  for (let key in json) {
-    const input = formMiCuenta.querySelector(`[name="${key}"]`);
-    if (input) {
-      input.value = json[key];
-    }
-  }
-}
+});
